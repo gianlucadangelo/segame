be5522026acce8d7bf706702daea98d2f2a88f37
ShaikhTahir <48475177+ShaikhTahir@users.noreply.github.com>
2019-05-24 14:14:08 +0200

    Ausgabe angepasst

--- SE/src/game/SpielUI.java
+++ SE/src/game/SpielUI.java
@@ -5,22 +5,41 @@ import java.util.Scanner;
 public class SpielUI {
 
 	public static void main(String[] arg) {
-		System.out.println("Spielen Ja oder nein? ");
-		Scanner sc = new Scanner(System.in);
+		System.out.println("Willkommen. " + "\n" + "Hauptmenü:");
+		System.out.println("(1) Spiel starten");
+		System.out.println("(2) Spiel beenden");
 
-		String was = sc.nextLine();
+		String abfrage;
+		Scanner sc = new Scanner(System.in);
+		boolean nochmal = true;
+		do {
+			abfrage = sc.nextLine();
+			switch (abfrage) {
+			case "1":
+				System.out.println("Das Spiel wird gestartet."+"\n");
+				nochmal = false;
+				break;
+			case "2":
+				nochmal = false;
+				break;
+			default:
+				System.out.println("Fehlerhafte Eingabe, bitte 1 oder 2 eingeben.");
+				nochmal = true;
+				break;
+			}
+		} while (nochmal);
+
 		// String was = "ja";
-		if (was.toLowerCase().equals("ja")) {
+		if (abfrage.equals("1")) {
 			boolean game = true;
-			System.out.println("Spieler1");
+			System.out.println("Name für Spieler 1 eingeben. Falls KI gewünscht, bitte KI eingeben.");
 			String spieler1, spieler2;
 			spieler1 = sc.nextLine();
-			System.out.println("Spieler2");
-			spieler2 = sc.nextLine();
+			
 			Spiel sp = new Spiel();
 
 			if (spieler1.toLowerCase().contains("ki")) {
-				System.out.println("Auswahl Schwierigkeitsgrad für" + spieler1 + ":" + "\n"
+				System.out.println("Auswahl Schwierigkeitsgrad für " +spieler1 + ":" + "\n"
 						+ "Bitte normal bzw. schwer eingeben.");
 				String mode = sc.nextLine();
 				boolean korrekt = false;
@@ -34,17 +53,21 @@ public class SpielUI {
 						Spiel.addSpieler(cp);
 						korrekt = true;
 					} else {
-						System.out.println("falsche Eingabe, bitte erneut versuchen.");
+						System.out.println("falsche Eingabe, bitte normal oder schwer eingeben.");
 
 					}
 				} while (!korrekt);
 
 			} else {
-				Mensch mensch = new Mensch("1#" + spieler1, 0);
+				Mensch mensch = new Mensch("1# " + spieler1, 0);
 				Spiel.addSpieler(mensch);
 			}
+			
+			System.out.println("Name für Spieler 2 eingeben. Falls KI gewünscht, bitte KI eingeben.");
+			spieler2=sc.nextLine();
+			
 			if (spieler2.toLowerCase().contains("ki")) {
-				System.out.println("Auswahl Schwierigkeitsgrad für" + spieler2 + ":" + "\n"
+				System.out.println("Auswahl Schwierigkeitsgrad für " + spieler2 + ":" + "\n"
 						+ "Bitte normal bzw. schwer eingeben.");
 				String mode = sc.nextLine();
 				boolean korrekt = false;
@@ -97,7 +120,7 @@ public class SpielUI {
 						}
 
 					}else {
-						System.out.println("Gib spalte an");
+						System.out.println("Wählen Sie die gewünschte Spalte aus.");
 						int spalte = sc.nextInt() - 1;
 						sp.ziehe(sp.gibSpieler(), spalte);
 					}
@@ -107,13 +130,13 @@ public class SpielUI {
 					sp.getStatus();
 					sp.changeSpieler();
 				} else {
-					System.out.println("Spielende!");
+					System.out.println("Spiel wird beendet");
 				}
 
 			}
 
 		} else {
-			System.out.println("Spiel beendet");
+			System.out.println("Spiel wird beendet");
 		}
 
 		sc.close();
